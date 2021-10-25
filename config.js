096a7416d775be71a17ec41ecb0fe49fb11c0a8c
Pawel Szmyd <lurid.pszmyd@gmail.com>
2021-10-25 21:24:27 +0200

    config db

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: "mongodb+srv://admin:22E3pgZcklXtWvo0@cluster0.g8gux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  keySession: ["KLUCZUNIKAT"],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+};
